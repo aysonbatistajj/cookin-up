e91a8f9f51795e92a24656e133dc0df96b028a3e
Ayson Rodrigues <19884@bemol.local>
2025-01-28 11:39:02 -0400

    components adicionados

--- src/components/CardCategoria.vue
+++ src/components/CardCategoria.vue
@@ -1,30 +1,32 @@
 <script lang="ts">
 import type ICategoria from '@/interfaces/ICategoria';
 import type { PropType } from 'vue';
+import Tag from './Tag.vue';
 
 export default {
-    props: {
-        categoria: { type: Object as PropType <ICategoria>, required: true }
-    }
+  props: {
+    categoria: { type: Object as PropType<ICategoria>, required: true }
+  },
+  components: { Tag }
 }
 </script>
 
 <template>
-    <article class="categoria">
-        <header class="categoria__cabecalho">
-            <img :src="`/imagens/icones/categorias_ingredientes/${categoria.imagem}`" alt="" class="categoria__imagem">
+  <article class="categoria">
+    <header class="categoria__cabecalho">
+      <img :src="`/imagens/icones/categorias_ingredientes/${categoria.imagem}`" alt="" class="categoria__imagem">
 
-            <h2 class="paragrafo-lg categoria__nome">{{ categoria.nome }}</h2>
+      <h2 class="paragrafo-lg categoria__nome">{{ categoria.nome }}</h2>
 
-        </header>
+    </header>
 
-        <ul class="categoria__ingredientes">
-            <li v-for="ingrediente in categoria.ingredientes" :key="ingrediente">
-                {{ ingrediente }}
-            </li>
-        </ul>
+    <ul class="categoria__ingredientes">
+      <li v-for="ingrediente in categoria.ingredientes" :key="ingrediente">
+        <IngredienteSelecionavel :ingrediente="ingrediente" />
+      </li>
+    </ul>
 
-    </article>
+  </article>
 </template>
 
 <style scoped>
@@ -65,5 +67,4 @@ export default {
   gap: 0.5rem;
   flex-wrap: wrap;
 }
-
 </style>
