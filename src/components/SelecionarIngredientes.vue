7d651807a1b3895c3ecbfb622ba1d3a6800acabf
Ayson Rodrigues <19884@bemol.local>
2025-02-04 11:17:15 -0400

    Componentes adicionados e ajuste de código

--- src/components/SelecionarIngredientes.vue
+++ src/components/SelecionarIngredientes.vue
@@ -2,8 +2,10 @@
 import { obterCategorias } from '@/http/index';
 import type ICategoria from '@/interfaces/ICategoria';
 import CardCategoria from './CardCategoria.vue';
+import BotaoPrincipal from './BotaoPrincipal.vue';
 
 export default {
+  name: 'SelecionarIngredientes',
   data() {
     return {
       categorias: [] as ICategoria[]
@@ -12,8 +14,8 @@ export default {
   async created() {
     this.categorias = await obterCategorias();
   },
-  components: { CardCategoria },
-  emits: ['adicionarIngrediente']
+  components: { CardCategoria, BotaoPrincipal },
+  emits: ['adicionarIngrediente', 'removerIngrediente', 'buscarReceitas']
 }
 </script>
 
@@ -30,6 +32,7 @@ export default {
         <CardCategoria 
          :categoria="categoria"
          @adicionar-ingrediente="$emit('adicionarIngrediente', $event)"
+         @remover-ingrediente="$emit( 'removerIngrediente', $event)"
          />
       </li>
     </ul>
@@ -37,6 +40,9 @@ export default {
     <p class="paragrafo dica">
       *Atenção: consideramos que você tem em casa sal, pimenta e água.
     </p>
+
+    <BotaoPrincipal texto="Buscar receitas!" @click="$emit('buscarReceitas')"/>
+
   </section>
 </template>
 
